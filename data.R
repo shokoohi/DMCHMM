0394ea4ff61bfe0fa4127fba78b1fde59be43885
shokoohi <shokoohi@icloud.com>
2017-08-08 12:29:07 -0400

    Delete data.R

--- data.R
+++
@@ -1,9 +0,0 @@
-#' @title data
-#' @description BS-Seq data for three cell type
-#' @author Farhad Shokoohi <shokoohi@icloud.com>
-#' @name data
-#' @format BED files
-#' @source Genomic Quebec
-#' @details The data is part of whole blood from Sweeden.
-#' @docType data
-NULL
